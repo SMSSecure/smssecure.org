ea4e8d9e0ac8f8d6357932a158e818f886961d80
Bastien Le Querrec <bastien@silence.im>
2019-09-26 15:45:20 +0200

    do not use async

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var express = require('express'),
 	request = require('request'),
 	commit = null,
 	moment = require('moment'),
-	fs = require('fs'),
-	async = require('async');
+	fs = require('fs');
 
 var app = express();
 
@@ -35,33 +34,21 @@ app.use(i18n.init);
 app.use(express.static(__dirname + '/assets'));
 app.set('view engine', 'ejs');
 
-updateCache = function(cb){
-	async.parallel({
-		commits: function(callback){
-			request('https://git.silence.dev/api/v4/projects/20/repository/commits', {timeout: parseInt(process.env.TIMEOUT) || 2000, headers: {'User-Agent': 'Silence Website'}}, function (err, res) {
-				if (err || typeof res == 'undefined' || typeof res.statusCode == 'undefined' || res.statusCode != 200) return callback(true);
-				fs.writeFile('./cache-commits.json', res.body, function (err) {
-					if (err) return callback("Cannot write cache-commits.json");
-					return callback(null, JSON.parse(res.body));
-				});
-			});
-		}
-	}, function(err, json){
-		return cb(err, json);
+updateCache = function(callback){
+	request('https://git.silence.dev/api/v4/projects/20/repository/commits', {timeout: parseInt(process.env.TIMEOUT) || 2000, headers: {'User-Agent': 'Silence Website'}}, function (err, res) {
+		if (err || typeof res == 'undefined' || typeof res.statusCode == 'undefined' || res.statusCode != 200) return callback(true);
+		fs.writeFile('./cache-commits.json', res.body, function (err) {
+			if (err) return callback("Cannot write cache-commits.json");
+			return callback(null, JSON.parse(res.body));
+		});
 	});
 };
 
-getCache = function(cb){
-	async.parallel({
-		commits: function(callback){
-			fs.readFile('./cache-commits.json', function (err, data) {
-				if (err) return callback(err);
-				return callback(null, JSON.parse(data));
-			});
-		}
-	}, function(err, json){
-		return cb(err, json);
-	})
+getCache = function(callback){
+	fs.readFile('./cache-commits.json', function (err, data) {
+		if (err) return callback(err);
+		return callback(null, JSON.parse(data));
+	});
 };
 
 getCacheTime = function(callback){
@@ -95,14 +82,14 @@ app.get('/', function (req, res) {
 
 	moment.locale(res.locale);
 
-	getData(function(err, json){
+	getData(function(err, commits){
 		if (err) console.log(err);
 		if (!err){
 			commit = {};
-			commit.message = json.commits[0].message.split('\n')[0];
-			commit.date = moment(json.commits[0].authored_date).fromNow();
-			commit.author = json.commits[0].author_name;
-			commit.link = 'https://git.silence.dev/Silence/Silence-Android/commit/'+json.commits[0].id;
+			commit.message = commits[0].message.split('\n')[0];
+			commit.date = moment(commits[0].authored_date).fromNow();
+			commit.author = commits[0].author_name;
+			commit.link = 'https://git.silence.dev/Silence/Silence-Android/commit/'+commits[0].id;
 		}
 		return res.render('index', {req: req, res: res, commit: commit});
 	});
