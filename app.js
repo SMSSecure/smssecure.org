ac4a2eb25f09448f716328e43af1bd259ffcce85
Bastien Le Querrec <bastien@silence.im>
2020-01-08 14:37:06 +0100

    fix missing donate path

--- app.js
+++ app.js
@@ -114,6 +114,10 @@ app.get('/privacy', function(req, res) {
 	return res.redirect('/#privacy');
 });
 
+app.get('/donate', function(req, res) {
+	return res.redirect('/#bitcoin');
+});
+
 var server = app.listen(parseInt(process.env.PORT) || 3000, function () {
 	console.log('App listening at http://%s:%s', server.address().address, server.address().port);
 })
